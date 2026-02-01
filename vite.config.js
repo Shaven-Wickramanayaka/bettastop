a78cc4604ded997268e5be555d1d1dcac2046725
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-02-01 18:00:21 +0000

    retry deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/bettastop/",
+  base: "./bettastop/",
   plugins: [vue(), tailwindcss()],
   rollupOptions: {
     input: {
